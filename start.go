a2fac534693cdb3bf70b19bb50bd17fc3fedb9b8
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2014-02-08 23:31:23 -0500

    Serving static assets

--- start.go
+++ start.go
@@ -12,6 +12,12 @@ func main() {
 	http.HandleFunc("/about/", webserver.AboutPageHandler)
 	http.HandleFunc("/user/", webserver.UserPageHandler)
 	http.HandleFunc("/upload", webserver.UploadPageHandler)
+	http.HandleFunc("/signup", webserver.SignUpPageHandler)
+	http.HandleFunc("/show", webserver.ShowUploadsHanlder)
+	// To serve a directory on disk (pwd/images) under an alternate URL
+	// path (/images/), use StripPrefix to modify the request
+	// URL's path before the FileServer sees it:
+	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("images"))))
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
