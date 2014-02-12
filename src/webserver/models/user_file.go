28b4b9d751a8cb16e6c183eed0095f8d36347e2a
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2014-02-11 23:18:57 -0500

    users files has tags no categories

--- src/webserver/models/user_file.go
+++ src/webserver/models/user_file.go
@@ -1,9 +1,9 @@
 package models
 
 type UserFile struct {
-	Title    string
-	Path     string
-	UserId   int64
-	Size     int64
-	Category string
+	Title  string
+	Path   string
+	UserId int64
+	Size   int64
+	Tags   []string
 }
